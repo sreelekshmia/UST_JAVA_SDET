5dbbc129f5042d1e9b5fde33ac0a84a3db8227e5
Sreelekshmi Ajithkumar <52187319+sreelekshmia@users.noreply.github.com>
2021-10-24 15:55:48 +0530

    Add files via upload

---
+++ TRAINING/PROTRACTOR/conf.js
@@ -0,0 +1,58 @@
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+var reporter = new HtmlScreenshotReporter({
+
+dest: 'target/screenshots',
+
+filename: 'my-report.html'
+
+});
+exports.config = {
+
+    directConnect: true,
+    
+    capabilities : {
+    
+    'browserName': 'chrome'
+    
+    },
+    framework: 'jasmine',
+
+specs: ['../Specs/spec_2.js'],
+
+jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+
+},
+// Setup the report before any tests start
+
+beforeLaunch: function() {
+
+    return new Promise(function(resolve){
+    
+    reporter.beforeLaunch(resolve);
+    
+    });
+    
+    },
+    // Assign the test reporter to each running instance
+
+onPrepare: function() {
+
+    jasmine.getEnv().addReporter(reporter);
+    
+    },
+    
+    // Close the report after all tests finish
+    
+    afterLaunch: function(exitCode) {
+    
+    return new Promise(function(resolve){
+    
+    reporter.afterLaunch(resolve.bind(this, exitCode));
+    
+    });
+    
+    }
+    
+    };
